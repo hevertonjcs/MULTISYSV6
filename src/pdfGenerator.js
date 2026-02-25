c803806f0b0198af1fba2890983bc79618813bc2
hevertonjcs <heverton167cristaldo@gmail.com>
2026-02-25 20:37:11 -0300

    Update pdfGenerator.js

--- src/pdfGenerator.js
+++ src/pdfGenerator.js
@@ -18,7 +18,7 @@ export const generatePDF = async (formData, logoConfig = null) => {
   const footerColor = [150, 150, 150];
 
   const multinegociacoesLogoUrl =
-    'https://bfxamibaxsyxltqkiftd.supabase.co/storage/v1/object/public/logocentral/logo.png';
+    'https://bfxamibaxsyxltqkiftd.supabase.co/storage/v1/object/public/logocentral/logooz.png';
 
   /* ----------------------------------------------------- */
   /* CONTROLE DE PÁGINA */
